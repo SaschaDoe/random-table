025315d830dcdcc472e3470fcf4303a6e72c9bd4
Sascha Dörflein <48054686+SaschaDoe@users.noreply.github.com>
2022-12-06 07:14:12 +0100

    refactor

--- src/routes/tables/character/disadvantage.md
+++ src/routes/tables/character/disadvantage.md
@@ -1,6 +1,6 @@
 ## Disadvantage Table
 ### evenly distributed role
-| Role | Name        | Description                                                          | Action           |
+| Role | Name        | Description                                                          | Actions          |
 |:----:|:------------|:---------------------------------------------------------------------|------------------|
 |  1   | Wanted from | The character is wanted and has to fear consequences if he is found. | AnotherCharacter |
 |  2   | Text        | And more                                                             |                  |
--- src/routes/tables/tables-loader.test.ts
+++ src/routes/tables/tables-loader.test.ts
@@ -1,26 +1,42 @@
-import { describe, it, expect, test } from 'vitest';
+import {expect, test } from 'vitest';
 import {TableParser} from "./table-parser";
 
-    const disadvantageTable =
-        `## Disadvantage Table
-         ### evenly distributed
-         | Role | Name        | Description   `;
-
-    const plotTable =
-        `## Plot Table
-         ### evenly distributed
-         | Role | Name        | Description   `;
-
     test.each([
-        [plotTable, "Plot Table", "evenly distributed"],
-        [disadvantageTable, "Disadvantage Table", "evenly distributed"],
+        [plotTableInput(), plotExpected()],
+        [disadvantageTableInput(), disadvantageExpected() ],
     ])('parse %s', (
         rawTable,
-        title,
-        distribution) => {
+        expected) => {
 
         let table = new TableParser().Parse(()=>rawTable);
 
-        expect(table.title).toBe(title);
-        expect(table.distribution).toBe(distribution);
+        expect(table.title).toBe(expected[0]);
+        expect(table.distribution).toBe(expected[1]);
     });
+
+    function plotTableInput(){
+        return `## Plot Table
+         ### evenly distributed
+         | Role | Name           | Description                                                                        | Example                                                                  |
+         |:----:|:---------------|:-----------------------------------------------------------------------------------|--------------------------------------------------------------------------|
+         |  1   | alien invasion | Show signs of it first, then indirect damage and only at the end the alien itself. | [War of the worlds](https://en.wikipedia.org/wiki/The_War_of_the_Worlds) |
+         |  2   | no plot        | No coherent plot                                                                   |                                                                          |
+         `;
+    }
+
+    function plotExpected(){
+        return ["Plot Table", "evenly distributed"]
+    }
+
+    function disadvantageTableInput(){
+        return `## Disadvantage Table
+         ### evenly distributed
+         | Role | Name        | Description             | Actions           |
+         |:----:|:------------|:------------------------|-------------------|
+         |  1   | Wanted from | The character is wanted | AnotherCharacter  |
+         |  2   | Ugly        | Ugly mainly in the face |                   |`;
+    }
+
+    function disadvantageExpected(){
+        return ["Disadvantage Table", "evenly distributed"];
+    }
